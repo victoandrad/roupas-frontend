70ba0afbe388c20b54dde3dbe0c9779ad5d2c313
Victor Andrade <victor64gaming@gmail.com>
2026-03-17 19:59:49 -0300

    Instalação da biblioteca de componentes PrimeVue

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config'
+import Aura from '@primeuix/themes/aura'
+import 'primeicons/primeicons.css'
 
 import App from './App.vue'
 import router from './router'
@@ -8,5 +11,10 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(PrimeVue, {
+	theme: {
+		preset: Aura,
+	},
+})
 
 app.mount('#app')
